2a16dd2511fad7401aeedbeade2a35d771389c19
Nicholas A Ciccarino <nciccarino@gmail.com>
2017-06-22 01:25:28 -0400

    more done

--- BasicCard.js
+++ BasicCard.js
@@ -1,31 +1,38 @@
 
 var inquirer = require("inquirer"); 
 var UserSearch = require("./UserSearch.js"); 
-var Questions = require("./questions.js");  
+var Questions = require("./questions.js"); 
 
-var argv = process.argv[3];  
+var arr = [];  
+var on = 0; 
 
 // UserSearch.prototype.printInfo = function() {
-// 	console.log(this.message + "\nYour answer: " + this.argv + "\nCorrect Answer: " + this.name);
+// 	console.log(this.message + "\nYour answer: " + this.name + "\nCorrect Answer: " + this.output.cloze);
 // }
 
 for (var i = 0; i < Questions.length; i++) {
 
 	var output = new UserSearch(Questions[i].text, Questions[i].cloze); 
-
+	arr.push(output);
 }
+
+//console.log(arr); 
+
 
 	inquirer.prompt([
 		{
 			type: "input",
 			name: "answer", 
-			message: output.partial 
+			message: arr[on].partial 
 		}
 
 	]).then(function(user){
 		if(user.answer === output.cloze) {
+			console.log("")
 			console.log("Correct!"); 
+			console.log("")
 			console.log(output.text); 
+			console.log("--------------------------------------------------------------------------------")
 		}
 
 		else {
@@ -39,9 +46,53 @@ for (var i = 0; i < Questions.length; i++) {
 
 		}
 
-		//var newInput = new UserSearch(user.message , user.argv, user.name);
+		// var newInput = new UserSearch(user.message , user.name, user.output.cloze);
 
-		//newInput.printInfo(); 
+		// newInput.printInfo(); 
 
 	}); //end then function
 
+//-----------------------------try later-----------------------------------------------------------------
+// for (var i = 0; i < Questions.length; i++) {
+// 	output();
+// }
+
+// function output(arr){
+// 	var arr = [new UserSearch(Questions[i].text, Questions[i].cloze)]; 
+// 	//console.log(arr[0]);  
+// 	var on = 0; 
+
+// 	inquirer.prompt([
+// 		{
+// 			type: "input",
+// 			name: "answer", 
+// 			message: arr[on].partial 
+// 		}
+
+// 	]).then(function(user){
+// 		if(user.answer === output.cloze) {
+// 			console.log("")
+// 			console.log("Correct!"); 
+// 			console.log("")
+// 			console.log(output.text); 
+// 			console.log("--------------------------------------------------------------------------------")
+// 		}
+
+// 		else {
+// 			console.log("")
+// 			console.log("Incorrect");
+// 			console.log(""); 
+// 			console.log("The correct answer was " + output.cloze); 
+// 			console.log(""); 
+// 			console.log(output.text); 
+// 			console.log("--------------------------------------------------------------------------------")
+
+// 		}
+
+// 		// var newInput = new UserSearch(user.message , user.name, user.output.cloze);
+
+// 		// newInput.printInfo(); 
+
+// 	}); //end then function
+
+// }//end function 
