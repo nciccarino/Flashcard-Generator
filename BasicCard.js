a7f5992e96015b516da1cc324d19b4b03231b28d
Nicholas A Ciccarino <nciccarino@gmail.com>
2017-06-22 22:47:44 -0400

    done

--- BasicCard.js
+++ BasicCard.js
@@ -5,6 +5,10 @@ var Questions = require("./questions.js");
 var arr = [];  
 var on = 0; 
 
+var yes = 0;
+var no = 0; 
+
+//pushes guestions through UserSearch and into an array (arr)
 for (var i = 0; i < Questions.length; i++) {
 	var output = new UserSearch(Questions[i].text, Questions[i].cloze); 
 	arr.push(output);
@@ -12,17 +16,15 @@ for (var i = 0; i < Questions.length; i++) {
 
 var askQuestions = function() {
 
-	if(on === arr.length) {
-		console.log("Game Over!"); 
-	} //end if statement 
+	// if the game isn't over...
+	if(on < arr.length) {
 
-	else {
 		inquirer.prompt([
-		{
-			type: "input",
-			name: "answer", 
-			message: arr[on].partial 
-		}
+			{
+				type: "input",
+				name: "answer",  
+				message: arr[on].partial 
+			}
 
 		]).then(function(user){
 
@@ -30,18 +32,23 @@ var askQuestions = function() {
 			var res = str.toLowerCase(); 
 
 			var str2 = arr[on].cloze
-			var res2 = str2.toLowerCase(); 
+			var res2 = str2.toLowerCase();   
 
-			if( res === res2 ) {
-				console.log("")
+			var yes = ("Correct"); 
+			var no = ("Incorrect"); 
+
+			if( res === res2 ) {  
+				yes++; 
+				console.log("");
 				console.log("Correct!"); 
 				console.log("")
 				console.log(arr[on].text); 
 				console.log("--------------------------------------------------------------------------------")
 			}
 
-			else {
-				console.log("")
+			else {  
+				no++; 
+				console.log("");
 				console.log("Incorrect");
 				console.log(""); 
 				console.log("The correct answer was " + arr[on].cloze); 
@@ -50,14 +57,45 @@ var askQuestions = function() {
 				console.log("--------------------------------------------------------------------------------")
 			}
 
-			on++; 
+			on++;  
 
-			askQuestions(); 
+			askQuestions(); //recursion
 
 		}); //end then function
 
+	} //end if statement 
+
+	//if the game is over... 
+	else if (on === arr.length) {
+		console.log("Game Over!");  
 	} //end else statement
 
 } //end askQuestions function 
 
-askQuestions(); 
+//start screen
+var start = function(){
+	inquirer.prompt([
+
+		{
+			type: "confirm",
+			message: "Do you want to play NFL Trivia?",
+			name: "yes"
+		}
+
+	]).then(function(user) {
+		if (user.yes) {
+			console.log("")
+			console.log(" NFL Trivia Game!" + "\n" +"\n Fill in the blank with the correct answer."); 
+			console.log("")
+			askQuestions();
+		}
+		else {
+			console.log(""); 
+			console.log(" Yeah...maybe next time..."); 
+			console.log(""); 
+		}
+	})
+}
+
+start(); 
+
